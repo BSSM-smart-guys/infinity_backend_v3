2b6d8a52cdbd4527ef6c8c8a3aa38c305b46f873
마현우 <mhw35689402@gmail.com>
2024-02-27 15:50:28 +0900

    CHORE :: global prefix 설정

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v3');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
