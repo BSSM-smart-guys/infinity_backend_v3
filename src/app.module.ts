ea95451a6746ac43fc9ff93ee9d19149ea63ae10
마현우 <mhw35689402@gmail.com>
2024-02-28 16:08:26 +0900

    REFACTOR :: prisma 모듈 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NovelModule } from './novel/novel.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     NovelModule,
-    PrismaModule,
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
