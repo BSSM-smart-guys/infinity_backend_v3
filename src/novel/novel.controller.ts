ec72312fe564d3d6df755a6dcc2291140d1e1fc7
마현우 <mhw35689402@gmail.com>
2024-02-29 11:08:00 +0900

    FEAT :: 소설 상세 조회 api 구현

--- src/novel/novel.controller.ts
+++ src/novel/novel.controller.ts
@@ -1,10 +1,24 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { NovelService } from './novel.service';
 import { CreateNovelDto } from '@/novel/dto';
+import { Novel } from '@prisma/client';
 
 @Controller('novel')
 export class NovelController {
   constructor(private readonly novelService: NovelService) {}
+
+  @Get(':id')
+  findById(@Param('id', new ParseIntPipe()) id: number): Promise<Novel> {
+    return this.novelService.findById(id);
+  }
 
   @Post()
   @HttpCode(201)
--- src/novel/novel.service.ts
+++ src/novel/novel.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNovelDto } from '@/novel/dto';
-import { PrismaClient } from '@prisma/client';
+import { Novel, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class NovelService {
+  async findById(id: number): Promise<Novel> {
+    const novel = await prisma.novel.findUnique({
+      where: {
+        uid: id,
+      },
+    });
+
+    return prisma.novel.update({
+      where: {
+        uid: id,
+      },
+      data: {
+        views: novel.views + 1,
+      },
+    });
+  }
+
   async createNovel({ title, content, thumbnail, category }: CreateNovelDto) {
     return (
       await prisma.novel.create({
