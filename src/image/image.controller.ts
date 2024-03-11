3d2f90341745d669aa21a75c875caa1292bac331
마현우 <mhw35689402@gmail.com>
2024-03-12 08:22:54 +0900

    FEAT :: 이미지 생성 컨트롤러에 AuthGuard 적용

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ImageService } from '@/image/image.service';
 import { ImageGenerateDto } from '@/image/dto/request';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   generateImage(@Body() { prompt }: ImageGenerateDto) {
     return this.imageService.generateImage(prompt);
   }
