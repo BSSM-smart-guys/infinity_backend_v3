fcee1e4ef7ceec24430fe77a9a4623aac5752097
hun1i <nicebrian0328@gmail.com>
2024-06-03 17:38:16 +0900

    REFACTOR :: auth부분 타입 설정 완료

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaClient, User } from '@prisma/client';
+import { TokenPayload } from './payload/tokenPayload.interface';
 
 const prisma = new PrismaClient();
 
@@ -14,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       secretOrKey: config().secret.key,
     });
   }
-  async validate(payload: any) {
+  async validate(payload: TokenPayload) {
     const user: User = await prisma.user.findUnique({
       where: { uid: payload.uid },
     });
